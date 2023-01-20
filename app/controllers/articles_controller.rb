d13cd2fb43c7bfb6b28812bdb621e459ed50366b
Victor Paim <victorspaim@hotmail.com>
2023-01-20 09:28:58 -0300

    chore: Removes new action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,6 @@ class ArticlesController < ApplicationController
     render json: @article, status: :ok
   end
 
-  def new
-    @article = Article.new
-  end
-
   def create
     @article = Article.new(article_params)
 
